16e0ea2f05612898e550a397afd7a9af531a452e
AlexeySpiridonov <forspall@gmail.com>
2012-07-18 23:51:00 +0400

    changes

--- clients/PHP/prflr.php
+++ clients/PHP/prflr.php
@@ -1,2 +1,42 @@
 <?
 
+class PRFLR {
+
+    private $timers;
+    private $delayedSend = false;
+    public static $group;
+    public static $thread;
+
+    public function __construct($group = false) {
+
+        if (!$group)
+            $this->group = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : 'Unknown';
+        else
+            $this->group = $group;
+    }
+
+    public static function Start($timer) {        
+        $this->timers[$timer] = microtime();
+    }
+
+    public static function Stop($timer, $info = '') {
+
+        if (!isset($this->timers[$timer]))
+            return false;
+
+        $delay = microtime() - $this->timers[$timer];
+
+
+
+        if (!$this->delayedSend) {
+            $this->send();
+        }
+    }
+
+    private static function send() {
+
+        unset($this->timers[$timer]);
+    }
+
+}
+
--- server/prflr.erl
+++ server/prflr.erl
@@ -1,18 +1,26 @@
 -module(prflr).
 -export([start/0]).
+-include ("/opt/Erlang/lib/erlang/lib/mongodb-master/include/mongo_protocol.hrl").
 
 start() ->
-    spawn(fun() -> server(4000) end).
+    spawn(fun() -> server(4000, localhost) end).
 
-server(Port) ->
+server(Port, MongoHost) ->
     {ok, Socket} = gen_udp:open(Port, [binary, {active, false}]),
-        io:format("Server opened socket:~p~n",[Socket]),
-    loop(Socket).
+    io:format("Server opened socket:~p~n",[Socket]),
 
-loop(Socket) ->
+    application:start(mongodb),
+    Host = {MongoHost, 27017},
+    {ok, Conn} = mongo:connect(Host),
+    io:format("Conn is : ~p~n", [Conn]),
+    DbConn = {test, Conn},
+
+    loop(Socket, DbConn).
+
+loop(Socket, DbConn) ->
     inet:setopts(Socket, [{active, once}]),
         receive
             {udp, Socket, Host, Port, Bin} ->
-            io:format("Server received:~p~n",[Bin]),
-        loop(Socket)
+                io:format("Server received:~p~n",[Bin]),
+                loop(Socket)
 end.
