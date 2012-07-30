862ad11b34e820d9529a2d71e079a1e7ed84cf19
Andrey Evsyukov <evsyukov@citypatrol.ru>
2012-07-31 01:05:49 +0400

    Profiler erlang server communicating with MongoDB

--- server/trash/prflr.erl
+++ server/trash/prflr.erl
@@ -1,41 +1,35 @@
 -module(prflr).
 -export([start/0]).
 -import(string, [tokens/2]).
-%%%-include ("./mongodb/include/mongo_protocol.hrl").
 
 start() ->
-    spawn(fun() -> server(4000, "188.127.227.36") end).
+    spawn(fun() -> server(4000, "127.0.0.1") end).
 
 server(Port, MongoHost) ->
     {ok, Socket} = gen_udp:open(Port, [binary, {active, false}]),
     io:format("Server opened socket:~p~n",[Socket]),
 
     application:start(mongodb),
+    application:start(bson),
     Host = {MongoHost, 27017},
     {ok, Conn} = mongo:connect(Host),
     io:format("Conn is : ~p~n", [Conn]),
-    DbConn = {test, Conn},
 
     loop(Socket, Conn).
-    %loop(Socket, MongoHost).
 
-loop(Socket, DbConn) ->
+loop(Socket, Conn) ->
     inet:setopts(Socket, [{active, once}]),
         receive
             {udp, Socket, Host, Port, Bin} ->
-                 io:format("Server received:~p~n",[Bin]),
-                %{ok, LastErr} = do(fun() ->  
-%makemessage(Bin),
-        {ok, Conn} = mongo:connect("188.127.227.36"),
-        mongo:do (safe, master, Conn, prflr, fun () ->
+                io:format("Server received:~p~n",[Bin]),
+		[Thread, Group, Timer, Duration, Info] = tokens(binary_to_list(Bin), "|"),
+        mongo:do (safe, master, Conn, prflr, fun() ->
+		%mongo:save(timers, {x,1,y,2})
 		Team = {thread, <<"1234567890">>, timer, <<"testtimer">>, group, <<"server">>, duration, <<"3">>, info, <<"test">>},
+		%%%Team = {thread,Thread, timer,Timer, group,Group, duration,Duration, info,Info},
 		mongo:insert(timers, Team)
-		
 	end),
-        mongo:disconnect (Conn),
-                   %mongo:insert(DbConn, "timers", [{"thread",'tread'}, {"group","Group"}, {"timer","Timer"}, {"duration","Duration"}, {"info","Info"}] ),
-                %end),       
-                loop(Socket, DbConn)
+                loop(Socket, Conn)
 end.
 
 stop(_State) ->
