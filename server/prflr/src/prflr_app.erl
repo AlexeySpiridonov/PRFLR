e2b93014727485129363001de3f65c3d8c158a1b
AlexeySpiridonov <forspall@gmail.com>
2012-08-06 00:03:42 +0400

    fixServerTrash

--- server/prflr/src/prflr_app.erl
+++ server/prflr/src/prflr_app.erl
@@ -7,44 +7,41 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
-%% include mongo
-%-include ("/usr/local/lib/erlang/lib/mongodb-master/include/mongo_protocol.hrl").
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
-start(_StartType, _StartArgs) ->
-    
-    spawn(fun() -> server(4000, "188.127.227.36") end),
+start(_StartType, _StartArgs) ->   
+    spawn(fun() -> server("127.0.0.1") end),
     prflr_sup:start_link().
 
-server(Port, MongoHost) ->
-    {ok, Socket} = gen_udp:open(Port, [binary, {active, false}]),
+server(MongoHost) ->
+    {ok, Socket} = gen_udp:open(4000, [binary, {active, false}]),
     io:format("Server opened socket:~p~n",[Socket]),
-
     application:start(mongodb),
+    application:start(bson),
     Host = {MongoHost, 27017},
     {ok, Conn} = mongo:connect(Host),
-    io:format("Conn is : ~p~n ", [Conn]),
-    DbConn = {test, Conn},
-    loop(Socket, DbConn).
+    io:format("Conn to mongo is : ~p~n", [Conn]),
+    loop(Socket, Conn).
 
-loop(Socket, DbConn) ->
+loop(Socket, Conn) ->
     inet:setopts(Socket, [{active, once}]),
         receive
             {udp, Socket, Host, Port, Bin} ->
-%%%             io:format("Server received:~p~n",[Bin]),
-                %{ok, LastErr} = do(fun() ->  
-                    mongo:insert(DbConn, "timers", makemessage(Bin) ),
-                %end),       
-                loop(Socket, DbConn)
+                io:format("Server received:~p~n",[Bin]),
+                mongo:do (safe, master, Conn, prflr, fun() ->                 
+                    mongo:insert(timers, makemessage(Bin))
+                end),      
+                loop(Socket, Conn)
 end.
 
 
 makemessage(Bin) ->
-    [Thread, Group, Timer, Duration, Info] = tokens(Bin, "|"),
-    [{"thread",Thread}, {"group",Group}, {"timer",Timer}, {"duration",Duration}, {"info",Info}].
+    [Thread, Group, Timer, Duration, Info] = tokens(binary_to_list(Bin), "|"),
+    {thread, <<"1234567890">>, timer, <<"testtimer">>, group, <<"server">>, duration, <<"3">>, info, <<"test">>}.
+
 
 stop(_State) ->
     ok.
