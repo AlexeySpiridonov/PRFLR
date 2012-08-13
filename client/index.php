54f5ae3d012c9edfee1e49f92931de7738a369b3
Andrey Evsyukov <evsyukov@citypatrol.ru>
2012-08-13 22:41:11 +0400

    #11, Timers format

--- client/index.php
+++ client/index.php
@@ -229,7 +229,7 @@
 <script type="text/javascript">
     function round(value)
     {
-	return Math.round(value*10000)/10000;
+	return Math.round(value*100)/100;
     }
     function formatNumber(number)
     {
