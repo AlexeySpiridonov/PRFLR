8d01b6aa7e03ebb54377b2f40d61b206783f1eab
AlexeySpiridonov <forspall@gmail.com>
2012-08-13 23:09:59 +0400

    favicon

--- langLibs/PHP/test.php
+++ langLibs/PHP/test.php
@@ -4,15 +4,15 @@ include('./prflr.php');
 
 // configure profiler
 //  // set  profiler server:port  and  set Group for timers 
-PRFLR::init('46.4.114.218', '4000', '1000msHost');
+PRFLR::init('46.4.114.218', '4000', '11msHost');
 
 PRFLR::Begin('checkUDP');
-for ($i = 0; $i < 1000; $i++) {
+for ($i = 0; $i < 100000; $i++) {
 //start timer
     $r = rand(1,9);
     PRFLR::Begin('test.timer'.$r);
-    sleep(1);
+    //sleep(1);
     PRFLR::End('test.timer'.$r, "step {$i}");
 }
-PRFLR::End('checkUDP','info');
+PRFLR::End('checkUDP');
 ?>
