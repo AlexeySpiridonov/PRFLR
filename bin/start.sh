5116ef182f8c016108510e24968064193d0fdb70
AlexeySpiridonov <forspall@gmail.com>
2013-04-24 23:27:30 +0400

    fix start

--- bin/start.sh
+++ bin/start.sh
@@ -2,9 +2,9 @@
 
 git pull
 
-ps aux | grep prflr | grep -v grep | awk '{print $2}' | xargs -i kill {}
-
 go build ./prflr.go
+
+ps aux | grep prflr | grep -v grep | awk '{print $2}' | xargs -i kill {}
 
 nohup ./prflr > /dev/null 2>&1 &
 
