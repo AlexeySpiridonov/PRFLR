f3809150cd9f318bd48592f279152846af8be75d
AlexeySpiridonov <forspall@gmail.com>
2013-04-24 22:45:43 +0400

    format source

--- bin/prflr.go
+++ bin/prflr.go
@@ -2,77 +2,76 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
-	"net"
-    "net/http"
-    "fmt"
-    "log"
-    "strings"
-	"strconv"
 	"labix.org/v2/mgo"
-    "labix.org/v2/mgo/bson"
-    "encoding/json"
+	"labix.org/v2/mgo/bson"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 /**
  * UDP Package struct
  */
 type Timer struct {
-    Thrd string
-    Src string
+	Thrd  string
+	Src   string
 	Timer string
-    Time float32
-    Info string
+	Time  float32
+	Info  string
 }
 
 /**
  * Web panel Struct
  */
 type Stat struct {
-    Src string
-    Timer string
-    Count int
-    Total float32
-    Min float32
-    Avg float32
-    Max float32
+	Src   string
+	Timer string
+	Count int
+	Total float32
+	Min   float32
+	Avg   float32
+	Max   float32
 }
 
 /**
  * Global variables
  */
 var (
-    dbName = "prflr"
-    dbHosts = "127.0.0.1"
-    dbCollection = "timers"
-    udpPort = ":4000"
-	httpPort = ":8080"
+	dbName                  = "prflr"
+	dbHosts                 = "127.0.0.1"
+	dbCollection            = "timers"
+	udpPort                 = ":4000"
+	httpPort                = ":8080"
 	cappedCollectionMaxByte = 100000000 // 100Mb
-	cappedCollectionMaxDocs = 300000 // 300k
+	cappedCollectionMaxDocs = 300000    // 300k
 )
-
 
 /* HTTP Handlers */
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("assets/main.html")
-    t.Execute(w,nil)
+	t.Execute(w, nil)
 }
 
 func lastHandler(w http.ResponseWriter, r *http.Request) {
 	db, err := mgo.Dial(dbHosts)
-    if err != nil {
-        log.Panic(err)
-    }
-    defer db.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
 
-    db.SetMode(mgo.Monotonic, true)
-    dbc := db.DB(dbName).C(dbCollection)
+	db.SetMode(mgo.Monotonic, true)
+	dbc := db.DB(dbName).C(dbCollection)
 
 	// Query All
 	var results []Timer
 
 	//TODO add criteria builder
-	err = dbc.Find( makeCriteria(r.FormValue("filter")) ).Sort("-_id").Limit(100).All(&results)
+	err = dbc.Find(makeCriteria(r.FormValue("filter"))).Sort("-_id").Limit(100).All(&results)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -83,9 +82,8 @@ func lastHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "%s", j)
 
-    db.Close()
+	db.Close()
 }
-
 
 func initDB() {
 	session, err := mgo.Dial(dbHosts)
@@ -104,7 +102,7 @@ func initDB() {
 	c.Create(&mgo.CollectionInfo{Capped: true, MaxBytes: cappedCollectionMaxByte, MaxDocs: cappedCollectionMaxDocs})
 
 	// Insert Test Datas
-	err = c.Insert(&Timer{Thrd:"1234567890", Timer: "prflr.check", Src: "test.src", Time: 1, Info: "test data"})
+	err = c.Insert(&Timer{Thrd: "1234567890", Timer: "prflr.check", Src: "test.src", Time: 1, Info: "test data"})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -113,14 +111,14 @@ func initDB() {
 
 func aggregateHandler(w http.ResponseWriter, r *http.Request) {
 	//TODO
-    db, err := mgo.Dial(dbHosts)
-    if err != nil {
-        log.Panic(err)
-    }
-    defer db.Close()
+	db, err := mgo.Dial(dbHosts)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
 
-    db.SetMode(mgo.Monotonic, true)
-    dbc := db.DB(dbName).C(dbCollection)
+	db.SetMode(mgo.Monotonic, true)
+	dbc := db.DB(dbName).C(dbCollection)
 
 	// Query All
 	var results []Stat
@@ -128,25 +126,25 @@ func aggregateHandler(w http.ResponseWriter, r *http.Request) {
 	grouplist := make(map[string]interface{})
 	groupparam := make(map[string]interface{})
 
-	grouplist["count"] = bson.M{"$sum":1}
-	grouplist["total"] = bson.M{"$sum":"$time"}
-	grouplist["min"]   = bson.M{"$min":"$time"}
-	grouplist["avg"]   = bson.M{"$avg":"$time"}
-	grouplist["max"]   = bson.M{"$max":"$time"}
+	grouplist["count"] = bson.M{"$sum": 1}
+	grouplist["total"] = bson.M{"$sum": "$time"}
+	grouplist["min"] = bson.M{"$min": "$time"}
+	grouplist["avg"] = bson.M{"$avg": "$time"}
+	grouplist["max"] = bson.M{"$max": "$time"}
 
 	q := strings.Split(r.FormValue("groupby"), ",")
 
 	if len(q) >= 1 && q[0] == "src" {
 		groupparam["src"] = "$src"
-		grouplist["src"]   = bson.M{"$first":"$src"}
+		grouplist["src"] = bson.M{"$first": "$src"}
 	}
 	if len(q) >= 2 && q[1] == "timer" {
-		grouplist["timer"] = bson.M{"$first":"$timer"}
+		grouplist["timer"] = bson.M{"$first": "$timer"}
 		groupparam["timer"] = "$timer"
 	}
 	grouplist["_id"] = groupparam
 	group := bson.M{"$group": grouplist}
-	sort  := bson.M{"$sort": bson.M{ r.FormValue("sortby"):-1 }}
+	sort := bson.M{"$sort": bson.M{r.FormValue("sortby"): -1}}
 	match := bson.M{"$match": makeCriteria(r.FormValue("filter"))}
 	aggregate := []bson.M{match, group, sort}
 
@@ -162,43 +160,41 @@ func aggregateHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "%s", j)
 
-    db.Close()
+	db.Close()
 }
-
 
 func makeCriteria(filter string) interface{} {
 	q := strings.Split(filter, "/")
 	c := make(map[string]interface{})
 
 	if len(q) >= 1 && q[0] != "" && q[0] != "*" {
-		c["src"]   = &bson.RegEx{Pattern: q[0]}
+		c["src"] = &bson.RegEx{Pattern: q[0]}
 	}
-	if len(q) >= 2 && q[1] != "" &&  q[1] != "*" {
+	if len(q) >= 2 && q[1] != "" && q[1] != "*" {
 		c["timer"] = &bson.RegEx{Pattern: q[1]}
 	}
-	if len(q) >= 3 && q[2] != "" &&  q[2] != "*" {
-		c["info"]  = &bson.RegEx{Pattern: q[2]}
+	if len(q) >= 3 && q[2] != "" && q[2] != "*" {
+		c["info"] = &bson.RegEx{Pattern: q[2]}
 	}
-	if len(q) >= 4 && q[3] != "" &&  q[3] != "*" {
+	if len(q) >= 4 && q[3] != "" && q[3] != "*" {
 		c["thrd"] = q[3]
 	}
 	return c
 }
 
-
 /* UDP Handlers */
-func  saveMessage(dbc *mgo.Collection, msg string) {
-	err:= dbc.Insert( prepareMessage(msg) )
-    if err != nil {
-        log.Panic(err)
-    }
+func saveMessage(dbc *mgo.Collection, msg string) {
+	err := dbc.Insert(prepareMessage(msg))
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
-func  prepareMessage(msg string) (timer Timer) {
-    fields := strings.Split(msg, "|")
+func prepareMessage(msg string) (timer Timer) {
+	fields := strings.Split(msg, "|")
 	//fmt.Println(fields)
 
-	time, err := strconv.ParseFloat(fields[3], 32);
+	time, err := strconv.ParseFloat(fields[3], 32)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -213,36 +209,35 @@ func main() {
 
 	/* Starting Web Server */
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
-	http.Handle("/favicon.ico", http.FileServer(http.Dir("./assets")))  //cool code for favicon! :)  it's very important!
+	http.Handle("/favicon.ico", http.FileServer(http.Dir("./assets"))) //cool code for favicon! :)  it's very important!
 	http.HandleFunc("/last/", lastHandler)
 	http.HandleFunc("/aggregate/", aggregateHandler)
 	http.HandleFunc("/", mainHandler)
 
-
 	go http.ListenAndServe(httpPort, nil)
 
 	/* Starting UDP Server */
-    //add here UDP aggregator  in  different thread
-    laddr, err := net.ResolveUDPAddr("udp", udpPort);
-    if err != nil {
+	//add here UDP aggregator  in  different thread
+	laddr, err := net.ResolveUDPAddr("udp", udpPort)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-    l, err := net.ListenUDP("udp", laddr);
+	l, err := net.ListenUDP("udp", laddr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// init Mongo connect
 	db, err := mgo.Dial(dbHosts)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-    // Optional. Switch the session to a monotonic behavior.
-    db.SetMode(mgo.Monotonic, true)
-    dbc := db.DB(dbName).C(dbCollection)
+	// Optional. Switch the session to a monotonic behavior.
+	db.SetMode(mgo.Monotonic, true)
+	dbc := db.DB(dbName).C(dbCollection)
 
 	// is Buffer enough?!?!
 	var buffer [500]byte
