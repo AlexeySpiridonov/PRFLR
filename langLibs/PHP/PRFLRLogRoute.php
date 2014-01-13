8f7a5ea538ee668cb31066d286665b4ec58d4c0b
Alexey Spiridonov <forspall@gmail.com>
2014-01-13 20:34:34 +0300

    fix php sdk

--- langLibs/PHP/PRFLRLogRoute.php
+++ langLibs/PHP/PRFLRLogRoute.php
@@ -10,8 +10,6 @@
  */
 class PRFLRLogRoute extends CLogRoute {
 
-    public $host;
-    public $port = 4000;
     public $source;
     public $apikey;
 
@@ -21,7 +19,7 @@ class PRFLRLogRoute extends CLogRoute {
     public function init() {
         parent::init();
         include_once(__DIR__ . '/prflr.php');
-        PRFLR::init($this->host, $this->port, $this->source, $this->apikey);
+        PRFLR::init($this->source, $this->apikey);
     }
 
     /**
